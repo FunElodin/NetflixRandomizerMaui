c643d4ce15700e5667f9298cfca4e878b87a9fec
Alan N Martellotti <alanmartellotti1991@gmail.com>
2025-10-31 12:57:58 +0100

    Añadimos Syncfusion

--- NetflixRandomizer/MauiProgram.cs
+++ NetflixRandomizer/MauiProgram.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
+using Syncfusion.Maui.Toolkit.Hosting;
 
 namespace NetflixRandomizer
 {
@@ -9,6 +10,7 @@ namespace NetflixRandomizer
             var builder = MauiApp.CreateBuilder();
             builder
                 .UseMauiApp<App>()
+                .ConfigureSyncfusionToolkit()
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
