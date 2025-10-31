b6285d7e0c29f3e623cd796961bbca90f87905d2
Alan N Martellotti <alanmartellotti1991@gmail.com>
2025-10-31 12:46:27 +0100

    Codigo Base Maui

---
+++ NetflixRandomizer/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace NetflixRandomizer
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ NetflixRandomizer/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "NetflixRandomizer")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "NetflixRandomizer.Pages")]
